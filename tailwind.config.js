dc45341a107c3817873836e8f33fb940626b46bc
rjkaz <rjkaz@comcast.net>
2021-05-18 01:01:49 -0400

    own color pallete

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./dist/**/*.html'],
+    },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    debugScreens: {
+      position: ['top', 'left'],
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: colors.white,
+        black: colors.black,
+        blue: colors.blue,
+        yellow: colors.yellow,
+        gray: colors.blueGray,
+        turquoise: colors.cyan,
+        green: colors.lime,
+        red: colors.rose,
+        },
+    extend: {
+      fontFamily: {
+        headline: ['Oswald']
+        },
+      colors: {
+        mainColor: '#1E293B'
+        
+      },
+     
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('tailwindcss-debug-screens'),
+  ],
+}
